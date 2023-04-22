9e067506e8cb461058f5d6ff78400bc322b8ed47
Jarturog <69714460+Jarturog@users.noreply.github.com>
2023-04-22 17:07:28 +0200

    web

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the monkey image element
+var monkeyImg = document.getElementById("monkey");
+
+// Add an event listener to the monkey image
+monkeyImg.addEventListener("click", function() {
+
+});
